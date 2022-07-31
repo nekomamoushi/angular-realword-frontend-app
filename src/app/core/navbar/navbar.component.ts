920be4fd24f88d02c139d4a3a6260e343a6626b2
germione <germione.grangere@gmail.com>
2022-07-31 09:29:45 +0200

    feat(core): display nav links if user is loggedIn or not

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { tap } from 'rxjs';
+import { AuthService } from 'src/app/features/auth/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
+  isLoggedIn$ = this.auth.isLoggedIn$.pipe(tap(console.log));
 
-  constructor() { }
+  constructor(private auth: AuthService) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
