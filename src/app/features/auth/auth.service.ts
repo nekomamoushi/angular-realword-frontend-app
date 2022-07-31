3cbdeb6e73015f9ebde7cdf2d7158c0877fa77e4
germione <germione.grangere@gmail.com>
2022-07-31 09:27:37 +0200

    feat(feature): save token to local storage on login or register

--- src/app/features/auth/auth.service.ts
+++ src/app/features/auth/auth.service.ts
@@ -1,30 +1,88 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, map, Observable } from 'rxjs';
+import { User, UserResponse } from 'src/app/core/models/user';
 import { environment } from '../../../environments/environment';
+
+const CURRENT_USER_KEY = 'currentUser';
+
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   baseUrl = `${environment.apiUrl}/users`;
+  private currentUserSubject = new BehaviorSubject<User | null>(null);
+  public currentUser$ = this.currentUserSubject.asObservable();
+  public isLoggedIn$ = this.currentUser$.pipe(map((user) => !!user));
 
-  constructor(private http: HttpClient) {}
-
-  register(username: string, email: string, password: string) {
-    return this.http.post(`${this.baseUrl}`, {
-      user: {
-        username,
-        email,
-        password,
-      },
-    });
+  constructor(private http: HttpClient) {
+    const currentUser = this.getUserFromLocalStorage();
+    this.currentUserSubject.next(currentUser);
   }
 
-  login(email: string, password: string) {
-    return this.http.post(`${this.baseUrl}/login`, {
-      user: {
-        email,
-        password,
-      },
-    });
+  get currentUser() {
+    return this.currentUserSubject.value;
+  }
+
+  register(
+    username: string,
+    email: string,
+    password: string
+  ): Observable<User> {
+    return this.http
+      .post<UserResponse>(`${this.baseUrl}`, {
+        user: {
+          username,
+          email,
+          password,
+        },
+      })
+      .pipe(
+        map((response: UserResponse) => {
+          this.saveUserToLocalStorage(response.user);
+          this.currentUserSubject.next(response.user);
+          return response.user;
+        })
+      );
+  }
+
+  login(email: string, password: string): Observable<User> {
+    return this.http
+      .post<UserResponse>(`${this.baseUrl}/login`, {
+        user: {
+          email,
+          password,
+        },
+      })
+      .pipe(
+        map((response: UserResponse) => {
+          this.saveUserToLocalStorage(response.user);
+          this.currentUserSubject.next(response.user);
+          return response.user;
+        })
+      );
+  }
+
+  logout() {
+    this.removeUserFromLocalStorage();
+    this.currentUserSubject.next(null);
+  }
+
+  private saveUserToLocalStorage(user: User): void {
+    localStorage.setItem(CURRENT_USER_KEY, JSON.stringify(user));
+  }
+
+  private getUserFromLocalStorage(): User | null {
+    const currentUser = localStorage.getItem(CURRENT_USER_KEY);
+
+    if (!currentUser) {
+      return null;
+    }
+
+    return JSON.parse(currentUser);
+  }
+
+  private removeUserFromLocalStorage(): void {
+    localStorage.removeItem(CURRENT_USER_KEY);
   }
 }
