320485e6bad01868e3b546af0ec4a3e19c84d41c
germione <germione.grangere@gmail.com>
2022-07-31 20:32:52 +0200

    feat(feature): display profile information

--- src/app/features/profile/components/profile/profile.component.ts
+++ src/app/features/profile/components/profile/profile.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
+import { Profile } from 'src/app/core/models/profile';
+import { ProfileService } from 'src/app/core/services/profile.service';
+import { AuthService } from 'src/app/features/auth/auth.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
+  currentProfile: Profile | null = null;
+  isUserConnected$ = this.auth.currentUser$.pipe(
+    map((currentUser) => {
+      return currentUser?.username === this.currentProfile?.username;
+    })
+  );
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private auth: AuthService,
+    private profile: ProfileService
+  ) {}
 
   ngOnInit(): void {
+    const username = this.route.snapshot.paramMap.get('username') as string;
+    this.profile.getProfile(username).subscribe((profile: Profile) => {
+      this.currentProfile = profile;
+    });
   }
-
 }
